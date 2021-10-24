99e08c89270fb82f2892acba7919943842db804d
Vinicius Sueiro <hi@vsueiro.com>
2021-10-24 11:11:56 -0300

    Creates apparent depth (smaller items are slower and further to the back)

--- new/js/script.js
+++ new/js/script.js
@@ -18,11 +18,18 @@
 		// Randomizes horizontal position
 		track.style.left = random( 0, 100 ) + '%'
 
+		// Randomizes apparent depth
+		let depth = random( .5, 1 )
+		track.querySelector( '.showcase-item' ).style.transform = 'translate( -50%, -50% ) scale(' + depth +')'
+
 		// Calculates animation speed
-		track.style.animationDuration = random( 5, 10 ) + 's'
+		track.style.animationDuration = 16 * ( 1 - depth + .5 ) + 's'
+
+		// Calculates layer order
+		track.style.zIndex = Math.round( depth * 1000 )
 
 		// Randomizes animation delay, in seconds
-		let delay = random( 0, 5 )
+		let delay = random( 0, 8 )
 
 		// Begins animation after delay
 		setTimeout( () => {
