ea15340ee18930d97744f7cbc194d93a832b3b2f
Vinicius Sueiro <hi@vsueiro.com>
2021-10-24 10:51:58 -0300

    Implements iterations with random properties

--- new/js/script.js
+++ new/js/script.js
@@ -3,29 +3,31 @@
 	// Generates random float in a range
 
 	let random = ( min, max ) => {
+
 		return Math.random() * ( max - min ) + min
+
 	}
 
 	// Animates showcase
 
 	let animate = ( track ) => {
 
-		// Pauses animation
-		track.style.animationPlayState = 'paused'
+		// Removes animation
+		track.style.animationName = ''
 
 		// Randomizes horizontal position
 		track.style.left = random( 0, 100 ) + '%'
 
 		// Calculates animation speed
-		track.style.animationDuration = random( 2, 6 ) + 's';
+		track.style.animationDuration = random( 5, 10 ) + 's'
 
 		// Randomizes animation delay, in seconds
-		let delay = random( 1, 6 )
+		let delay = random( 0, 5 )
 
 		// Begins animation after delay
 		setTimeout( () => {
 
-			track.style.animationPlayState = 'running'
+			track.style.animationName = 'float'
 
 		}, delay * 1000 )
 
@@ -39,21 +41,7 @@
 		animate( track )
 
 		// Shuffles animation properties each time it runs
-		// track.addEventListener( 'animationiteration', () => { animate( event.target ) } )
-
-		// Pauses animation on hover
-
-		let item = track.querySelector( '.showcase-item' )
-
-		item.addEventListener( 'mouseover', () => {
-			let track = item.parentElement
-			track.style.animationPlayState = 'paused'
-		} )
-
-		item.addEventListener( 'mouseout', () => {
-			let track = item.parentElement
-			track.style.animationPlayState = 'running'
-		} )
+		track.addEventListener( 'animationend', () => { animate( event.target ) } )
 
 	}
 
