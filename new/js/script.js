f3099deff41bf7fb114dd8e37e883fbc7d2883b1
Vinicius Sueiro <hi@vsueiro.com>
2021-10-28 21:24:37 -0300

    Enables click on project thumbnail (like if the whole thing was a link)

--- new/js/script.js
+++ new/js/script.js
@@ -242,4 +242,35 @@
 
 	}
 
+	{ // Enables click on project thumbnail
+
+		// Gathers all items from the works list
+		let items = document.querySelectorAll( '.works li' )
+
+		// Loops through them
+		for ( let item of items ) {
+
+			// Detects when the user clicks it
+			item.addEventListener( 'click', event => {
+
+				// Gets respective anchor
+				let anchor = item.querySelector( 'a' )
+
+				// Runs only if click was not on the anchor itself
+				if ( event.target !== anchor ) {
+
+					// Gets respective URL
+					let url = anchor.href
+
+					// Opens URL
+					window.open( url, '_self' )
+
+				}
+
+			} )
+
+		}
+
+	}
+
 })()
