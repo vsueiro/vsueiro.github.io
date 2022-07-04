081fb3812409ca8cd39eb5c014bd357fd3ee0f7e
Vinicius Sueiro <hi@vsueiro.com>
2022-07-04 09:23:35 -0300

    Splits JavaScript code into simple modules

---
+++ js/modules/entrance-fx.js
@@ -0,0 +1,32 @@
+// Entrance FX
+
+// Gathers all elements to apply the transition
+const elements = document.querySelectorAll( '.entrance, .flow > *, .works li' )
+
+// Defines arbitrary constant to calculate transition delay, in seconds
+const basis = 1
+
+// Loops through them
+for ( let order = 1; order < elements.length; order++ ) {
+
+  // Gets current element
+  let element = elements[ order ]
+
+  // Increases number to prevent delay of 0
+  let number = order + 1
+
+  // Calculates delay (log makes delay smaller each time)
+  let delay = basis * Math.log( number )
+
+  // Applies transition delay
+  element.style.transitionDelay = delay + 's'
+
+}
+
+// Defines arbitrary delay before triggering transitions
+setTimeout( () => {
+
+  // Triggers transitions
+  document.body.dataset.entrance = true
+
+}, 500 )
---
+++ js/modules/safari-fixes.js
@@ -0,0 +1,46 @@
+// Safari Fixes
+
+// Checks if browser is Safari
+const safari = /^((?!chrome|android).)*safari/i.test( navigator.userAgent )
+
+// Checks if system is iOS
+const ios = /iP(ad|od|hone)/i.test( window.navigator.userAgent )
+
+if ( ios && safari ) {
+
+  // Creates browser-specific attribute
+  document.body.dataset.browser = 'safari'
+
+  { // Fixes autoplay for (some) iPhones
+
+    // Checks if a video is currently playing
+    const playing = ( video ) => {
+
+      if ( video.currentTime > 0 && !video.paused && !video.ended && video.readyState > 2 )
+        return true
+
+    }
+
+    // Defines autoplay function
+    const autoplay = () => {
+
+      // Gathers all videos on the page
+      let videos = document.querySelectorAll( 'video' )
+
+      // Loops through them
+      for ( let video of videos ) {
+
+        // Plays the video (if it was supposed to be playing)
+        if ( video.hasAttribute( 'autoplay' ) && !playing( video ) )
+          video.play()
+
+      }
+
+    }
+
+    // Runs autoplay function every time users touch the screen
+    document.addEventListener( 'touchstart', autoplay )
+
+  }
+
+}
