a480217ce89018fd70f67e701eaa4d9e0ac6e975
Vinicius Sueiro <hi@vsueiro.com>
2024-01-27 12:58:46 -0500

    Redirect users to the work section of my homepage when they access vsueiro.com/work/

--- js/modules/entrance-fx.js
+++ js/modules/entrance-fx.js
@@ -6,11 +6,14 @@ const elements = document.querySelectorAll(".entrance, .flow > *, .works li");
 // Stores if reduced motion accessibility setting is on
 const reducedMotion = window.matchMedia("(prefers-reduced-motion)").matches;
 
-// Stores if page is already significantly scrolled  (e.g., if user reloads after scrolling past the “fold)
+// Stores if page is already significantly scrolled  (e.g., if user reloads after scrolling past the “fold”)
 const isScrolled = document.documentElement.scrollTop > window.innerHeight;
 
+// Stores if there is a hash (e.g., "#work") in the URL
+const hasHash = window.location.hash.length > 0;
+
 // Checks if accessibility setting is off and page is not scrolled
-if (!reducedMotion && !isScrolled) {
+if (!reducedMotion && !isScrolled && !hasHash) {
   // Defines arbitrary constant to calculate transition delay, in seconds
   let basis = 1;
 
