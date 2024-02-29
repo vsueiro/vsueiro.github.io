d75f99f7dfff3f19f5f73efc5e2a815bd7ed93c3
Vinicius Sueiro <hi@vsueiro.com>
2024-02-29 11:43:27 -0500

    Import assets and add basic meteor scene

--- experiments/meteor-destroyer/script.js
+++ experiments/meteor-destroyer/script.js
@@ -1,36 +1,45 @@
-class Example extends Phaser.Scene {
-  preload() {
-    this.load.setBaseURL("https://labs.phaser.io");
+let width = 1920;
+let height = 1080;
 
-    this.load.image("sky", "assets/skies/space3.png");
-    this.load.image("logo", "assets/sprites/phaser3-logo.png");
-    this.load.image("red", "assets/particles/red.png");
+class MeteorsScene extends Phaser.Scene {
+  preload() {
+    this.load.setBaseURL("./assets/");
+
+    this.load.image("background", "background.png");
+    this.load.image("meteor", "meteor.png");
+    this.load.image("meteor-trace", "meteor-trace.png");
+    this.load.image("mountains", "mountains.png");
   }
 
   create() {
-    this.add.image(400, 300, "sky");
+    const background = this.add.image(0, 0, "background");
+    background.setOrigin(0, 0);
 
-    const particles = this.add.particles(0, 0, "red", {
-      speed: 100,
+    const meteor = this.physics.add.image(100, 100, "meteor");
+    meteor.setBlendMode("ADD");
+    meteor.setVelocity(400, 100);
+    meteor.setBounce(0, 0);
+    meteor.setCollideWorldBounds(true);
+
+    const particles = this.add.particles(0, 0, "meteor-trace", {
+      speed: 10,
+      quantity: 5,
       scale: { start: 1, end: 0 },
+      alpha: { start: 0.5, end: 0 },
       blendMode: "ADD",
     });
+    particles.startFollow(meteor);
 
-    const logo = this.physics.add.image(400, 100, "logo");
-
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
-
-    particles.startFollow(logo);
+    const mountains = this.add.image(0, height - 72, "mountains");
+    mountains.setOrigin(0, 0);
   }
 }
 
 const config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  scene: Example,
+  width: width,
+  height: height,
+  scene: MeteorsScene,
   physics: {
     default: "arcade",
     arcade: {
