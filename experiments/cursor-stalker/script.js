9b91ce615f28077f7e6e9c173a162511fc8c13cf
Vinicius Sueiro <hi@vsueiro.com>
2024-06-06 01:55:40 -0400

    Replace lerp smoothing code by class-based version

--- experiments/cursor-stalker/script.js
+++ experiments/cursor-stalker/script.js
@@ -1,27 +1,44 @@
-let stalker;
+class Stalker {
+  constructor(decay) {
+    this.decay = decay || 12; // Useful range is approx. 1 (slow) 24 (fast)
+    this.element = document.createElement("div");
+    this.x = 0.5;
+    this.y = 0.5;
 
+    this.setup();
+  }
+
+  setup() {
+    this.element.classList.add("stalker");
+    document.body.append(this.element);
+  }
+
+  // By Freya Holmér https://youtu.be/LSNQuFEDOyQ
+  expDecay(a, b, decay, dt) {
+    return b + (a - b) * Math.exp(-decay * dt);
+  }
+
+  update(deltaTime) {
+    this.x = this.expDecay(this.x, targetX, this.decay, deltaTime);
+    this.y = this.expDecay(this.y, targetY, this.decay, deltaTime);
+
+    this.element.style.left = `${this.x * 100}%`;
+    this.element.style.top = `${this.y * 100}%`;
+  }
+}
+
+let stalkers = [];
 let lastTimestamp = 0;
 let targetX = 0;
 let targetY = 0;
-let stalkerX = 0;
-let stalkerY = 0;
 
-window.addEventListener("mousemove", function (event) {
+window.addEventListener("mousemove", (event) => {
   targetX = event.clientX / window.innerWidth;
   targetY = event.clientY / window.innerHeight;
 });
 
-// By Freya Holmér
-// https://youtu.be/LSNQuFEDOyQ
-
-function expDecay(a, b, decay, dt) {
-  return b + (a - b) * Math.exp(-decay * dt);
-}
-
 function setup() {
-  stalker = document.createElement("div");
-  stalker.classList.add("stalker");
-  document.body.append(stalker);
+  stalkers.push(new Stalker(16));
 }
 
 setup();
@@ -29,14 +46,9 @@ setup();
 function update(timestamp) {
   const deltaTime = (timestamp - lastTimestamp) / 1000;
 
-  // Useful range is approx. 1 (slow) 25 (fast)
-  const decay = 12;
-
-  stalkerX = expDecay(stalkerX, targetX, decay, deltaTime);
-  stalkerY = expDecay(stalkerY, targetY, decay, deltaTime);
-
-  stalker.style.left = `${stalkerX * 100}%`;
-  stalker.style.top = `${stalkerY * 100}%`;
+  for (let stalker of stalkers) {
+    stalker.update(deltaTime);
+  }
 
   lastTimestamp = timestamp;
   requestAnimationFrame(update);
