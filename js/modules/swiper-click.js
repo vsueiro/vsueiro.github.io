1097a951858064794c28888bebc2854223e8c5e0
Vinicius Sueiro <hi@vsueiro.com>
2025-08-30 23:14:27 -0300

    Add trio poster to NBA project and simplify image carousels (no click for now)

--- js/modules/swiper-click.js
+++ js/modules/swiper-click.js
@@ -1,18 +1,21 @@
-// Enables click to advance image carousel
+// // Enables click to advance image carousel
 
-// Gathers all <swiper-container> elements from the page
-const swiperElements = document.querySelectorAll("swiper-container");
+// // Gathers all <swiper-container> elements from the page
+// const swiperElements = document.querySelectorAll("swiper-container");
 
-// Loops through every swiper element on the page
-for (let swiperElement of swiperElements) {
-  // Detects when user clicks it
-  swiperElement.addEventListener("click", (event) => {
-    // Ignore clicks on caption
-    if (event.target.tagName === "FIGCAPTION") {
-      return;
-    }
+// // Loops through every swiper element on the page
+// for (let swiperElement of swiperElements) {
+//   // Detects when user clicks it
+//   swiperElement.addEventListener("click", (event) => {
 
-    // Goes to the next slide
-    swiperElement.swiper.slideNext();
-  });
-}
+//     console.log(event.target.tagName, event.target.classList)
+
+//     // Ignore clicks on caption
+//     if (event.target.tagName === "FIGCAPTION") {
+//       return;
+//     }
+
+//     // Goes to the next slide
+//     swiperElement.swiper.slideNext();
+//   });
+// }
